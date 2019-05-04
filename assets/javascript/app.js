9fdd743489983e68dfb8b613136cf212000dbd6a
jsevern94 <severnj@spu.edu>
2019-05-04 14:12:40 -0700

    added gif pause feature and styled buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
-
+    //array of countries to make into buttons
     var countries = ["Greece", "Germany", "Hungary", "Italy", "France", "Spain", "England", "Russia", "Finland", "Sweden", "Denmark"]
-
+    //function to create the buttons
     function createButtons() {
         $("#button-div").empty();
         for (var i = 0; i < countries.length; i++) {
             var newButton = $("<button>");
-            newButton.attr("class", "country-button")
+            newButton.attr("class", "country-button btn btn-success")
             newButton.attr("country", countries[i]);
             newButton.text(countries[i]);
             $("#button-div").append(newButton);
         }
     }
-
+    //function to create an on-click event to start/stop gifs
+    function gifClick() {
+        $("img").on("click", function () {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+    }
+    //function to get gifs and push them gif div
     $(document.body).on("click", ".country-button", function () {
         var search = $(this).attr("country");
         console.log(search);
@@ -25,39 +38,30 @@ $(document).ready(function () {
         })
             .then(function (response) {
                 var results = response.data;
+                console.log(results);
                 $("#gif-div").empty();
                 for (var i = 0; i < results.length; i++) {
-                    var gifDiv = $("<div>");
+                    var gifDiv = $("<div>").attr("class", "gif-div");
                     var rating = results[i].rating;
-                    var p = $("<p>");
-                    var personImage = $("<img>");
-                    p.text("Rating: " + rating);
-                    personImage.attr("src", results[i].images.fixed_height.url);
+                    var p = $("<p>").text("Rating: " + rating);
+                    var countryImage = $("<img>");
+                    countryImage.attr("src", results[i].images.fixed_height_still.url);
+                    countryImage.attr("data-still", results[i].images.fixed_height_still.url);
+                    countryImage.attr("data-animate", results[i].images.fixed_height.url);
+                    countryImage.attr("data-state", "still");
                     gifDiv.prepend(p);
-                    gifDiv.prepend(personImage);
+                    gifDiv.prepend(countryImage);
                     $("#gif-div").prepend(gifDiv);
                 }
+                gifClick();
             });
     });
-
-    // This function handles events where the add movie button is clicked
+    //function for creating new buttons
     $("#add-country").on("click", function (event) {
         event.preventDefault();
         countries.push($("#country-input").val());
         $("#country-input").val("");
         createButtons();
     });
-
     createButtons();
-
-
-
-    // if (state === "still") {
-    //     $(this).attr("src", $(this).attr("data-animate"));
-    //     $(this).attr("data-state", "animate");
-    // } else {
-    //     $(this).attr("src", $(this).attr("data-still"));
-    //     $(this).attr("data-state", "still");
-    // }
-
 });
